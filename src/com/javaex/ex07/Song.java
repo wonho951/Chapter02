7ee073ab3ecf1630c0ee68ac604c2c9a11340a22
wonho <wonho951@gmail.com>
2021-06-03 15:11:02 +0900

    2021.06.03 수업내용 생성자 여러개 만들어보기

--- src/com/javaex/ex07/Song.java
+++ src/com/javaex/ex07/Song.java
@@ -2,26 +2,31 @@ package com.javaex.ex07;
 
 public class Song {
 
-	//필드
-	private String title;
+	// 필드 
+	private String title; 
 	private String artist;
 	private String album;
 	private String composer;
 	private int year;
 	private int track;
+
+	// 생성자 
 	
-	
-	//생성자
-	public Song() {
-		
+	//상황 복잡하게 만들기 위해 생성자 하나 생성
+	public Song(String title, int track) {
+		this.title = title;
+		this.track = track;
 	}
 	
+	public Song() {
+	
+	  }
+
 	public Song(String artist, String title) {
 		this.artist = artist;
 		this.title = title;
 	}
-	
-	
+
 	public Song(String artist, String title, String album, int year, int track, String composer) {
 		this.artist = artist;
 		this.title = title;
@@ -39,64 +44,63 @@ public class Song {
 		this.track = track;
 		this.composer = composer;
 	}
-	
-	
-	//메소드(게터,세터)
-	//위 형식대로 생성자 해뒀을땐 굳이 게터 필요없음
-	public String getTitle() {
-		return title;
+
+	// 메소드(게터,세터) 
+	//위 형식대로 생성자 해뒀을땐 굳이 게터 필요없음 
+	public String getTitle() { return
+	title;
+
 	}
-	
+
 	public String getArtist() {
 		return artist;
 	}
-	
+
 	public String getAlbum() {
 		return album;
 	}
-	
+
 	public String getComposer() {
 		return composer;
 	}
-	
+
 	public int getYear() {
 		return year;
 	}
-	
+
 	public int getTrack() {
 		return track;
 	}
-	
-	
-	
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
 	public void setArtist(String artist) {
 		this.artist = artist;
 	}
-	
+
 	public void setAlbum(String album) {
 		this.album = album;
 	}
-	
+
 	public void setComposer(String composer) {
 		this.composer = composer;
 	}
-	
+
 	public void setYear(int year) {
 		this.year = year;
 	}
-	
+
 	public void setTrack(int track) {
 		this.track = track;
 	}
-	
-	
-	//메소드-일반
-	
+
+	// 메소드-일반
+
 	public void showInfo() {
-		System.out.println(artist + ", " +  title + " " + "( " + album + ", " + year + ", " + track + "번 track, " + composer + " 작곡 )");
+		System.out.println(artist + ", " + title + " " + "( " + album + ", " + year + ", " + track + "번 track, "
+				+ composer + " 작곡 )");
 	}
+
 }
--- src/com/javaex/ex07/SongApp.java
+++ src/com/javaex/ex07/SongApp.java
@@ -1,10 +1,10 @@
 package com.javaex.ex07;
-
+//import 있나 없나 확인하기. 같은 패키지에서는 굳이 불러 올 필요가 없음.
 public class SongApp {
 
 	public static void main(String[] args) {
 		
-
+		
 		Song Iu = new Song();
 		Iu.setArtist("아이유");
 		Iu.setTitle("좋은날");
@@ -12,7 +12,6 @@ public class SongApp {
 		Iu.setYear(2010);
 		Iu.setTrack(3);
 		Iu.setComposer("이민수");
-		
 		Iu.showInfo();
 		
 		
@@ -20,16 +19,15 @@ public class SongApp {
 		BigBang.showInfo();
 		
 		
-		Song Busker = new Song("버스커버스커", "벚꽃엔딩");
-		Busker.setAlbum("버스커버스커 1집");
-		Busker.setYear(2012);
-		Busker.setTrack(4);
-		Busker.setComposer("장범준");
+		Song Busker = new Song("버스커버스커", "벚꽃엔딩");		
 		Busker.showInfo();
 		
 		
 		Song iu = new Song("이민수", 3, 2010, "Real", "좋은날", "아이유");
 		iu.showInfo();
+		
+		   
+		
 	}
 
 }
