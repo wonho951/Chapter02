d93403e27bed33243c7eb5c0e82b0f9bcd9335e6
wonho <wonho951@gmail.com>
2021-06-03 10:39:35 +0900

    2021.06.03 수업내용 (생성자)

--- src/com/javaex/ex05/GoodsApp.java
+++ src/com/javaex/ex05/GoodsApp.java
@@ -4,7 +4,7 @@ public class GoodsApp {
 
 	public static void main(String[] args) {
 		
-		Goods camera = new Goods(); //---> Goods() 는 생성자를 부름
+		Goods camera = new Goods(); //---> Goods() 는 생성자를 부름(디폴트 생성자)
 		
 		camera.setName("니콘");
 		camera.setPrice(400000);
@@ -13,6 +13,7 @@ public class GoodsApp {
 		
 		
 		
+		Goods computer = new Goods("그램" , 1000000);
 		
 		
 	}
