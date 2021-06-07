1d6ede7bdd7a358afb35bd048e9403ff996b040f
wonho <wonho951@gmail.com>
2021-06-07 15:16:28 +0900

    Tv선생님 풀이 코드 확인해보기

--- src/com/javaex/ex08/Tv.java
+++ src/com/javaex/ex08/Tv.java
@@ -94,5 +94,110 @@ public class Tv {
 			}
 			System.out.println("");
 		}
+		
+		
+		/* 선생님 풀이
+		// 필드
+		private int channel, volume;
+		private boolean power;
+
+		// 생성자
+		public Tv() {
+		}
+
+		public Tv(int channel, int volume, boolean power) {
+			this.channel = channel;
+			this.volume = volume;
+			this.power = power;
+		}
+
+		// 메소드 getter
+		public int getChannel() {
+			return channel;
+		}
+
+		public int getVolume() {
+			return volume;
+		}
+
+		public boolean getPower() {
+			return power;
+		}
+
+		// 메소드 - 일반
+		public void power(boolean on) {
+			if (on == true) {
+				this.power = true;
+			} else {
+				this.power = false;
+			}
+		}
+
+		public void channel(int channel) {
+			if (this.power == true) { // TV가 켜져있을 때
+				if (channel > 255) {
+					this.channel = 255;
+				} else if (channel < 1) {
+					this.channel = 1;
+				} else {
+					this.channel = channel;
+				}
+			} else { 
+				System.out.println("전원이 꺼져 채널이 변경되지 않습니다.");
+			}
+		}
+
+		public void channel(boolean up) {
+			int nextCannel;
+			if(up == true) {
+				nextCannel = this.channel+1;
+			}else {
+				nextCannel = this.channel-1;
+			}
+			
+			this.channel(nextCannel);
+		}
+		
+
+		public void volume(int volume) {
+			if (this.power == true) {
+				if (volume > 100) {
+					this.volume = 100;
+				} else if (volume < 0) {
+					this.volume = 0;
+				} else {
+					this.volume = volume;
+				}
+			} else {
+				System.out.println("전원이 꺼져 볼륨이 변경되지 않습니다.");
+			}
+		}
+
+		public void volume(boolean up) {
+			int nextVolume;
+			if(up == true) {
+				nextVolume = this.volume+1;
+			}else {
+				nextVolume = this.volume-1;
+			}
+			
+			this.volume(nextVolume);
+			
+		}
+
+		
+		public void status() {
+			
+			String powerOn;
+			
+			if(this.power == true) {
+				powerOn = "on";
+			}else {
+				powerOn = "off";
+			}
+			
+			System.out.println("[Power]:" + powerOn + "\t[Channel]:" + this.channel + "\t[Volume]:" + this.volume);
+		
+		}*/
 	
 	}
--- src/com/javaex/ex08/TvApp.java
+++ src/com/javaex/ex08/TvApp.java
@@ -28,36 +28,110 @@ public class TvApp {
 		tv.power(false);  // 전원off
 		tv.status();      // tv상태
 			
+		
+		/* 선생님 풀이 코드 확인해보기.
+		// 초기값 학인
+		System.out.println("*초기값확인--------------------------------------------------");
+		Tv tv = new Tv(7, 20, true); //전원이 켜졌을때
+		//Tv tv = new Tv(7, 20, false);  //전원이 꺼졌을때
+		
+		tv.status(); 
+		
+		System.out.println("");
+		System.out.println("*채널 번호입력--------------------------------------------------");
+		// 채널확인-번호입력
+		tv.channel(222); // 채널 7-->222
+		tv.status(); 
+		
+		// 채널확인-번호입력
+		tv.channel(12); // 채널 222-->12
+		tv.status(); 
+		
+		// 채널확인-번호입력 최대값
+		tv.channel(256);  // 채널 12-->256-->255
+		tv.status(); 
+		
+		// 채널확인-번호입력 최소값
+		tv.channel(0);  // 채널 255-->0-->1
+		tv.status(); 		
+				
+		
+		System.out.println("");
+		System.out.println("*채널 up down--------------------------------------------------");
+		
+		tv.channel(133);
+		tv.status(); 
+		
+		// 채널확인-up
+		tv.channel(true); // 채널 133-->134
+		tv.status(); 
+		
+		// 채널확인-down
+		tv.channel(false); // 채널 134-->133
+		tv.status(); 
+		
+		// 채널확인-up 최대값
+		tv.channel(255); // 채널 134-->255
+		tv.status(); 
+		tv.channel(true); // 채널 255-->256-->255
+		tv.status();
+		
+		// 채널확인-down 최소값
+		tv.channel(1); // 채널 255-->1
+		tv.status(); 
+		tv.channel(false); // 채널 1-->0-->1
+		tv.status();
+				
+		System.out.println("");
+		System.out.println("*볼륨 번호입력--------------------------------------------------");
+		
+		// 볼륨확인-번호입력
+		tv.volume(99); // 볼륨 20-->99
+		tv.status(); 
+		
+		// 볼륨확인-번호입력
+		tv.volume(2); // 볼륨 99-->2
+		tv.status(); 
+		
+		// 볼륨확인-번호입력 최대값
+		tv.volume(102); // 볼륨 2-->102-->100
+		tv.status(); 
+
+		// 볼륨확인-번호입력 최소값
+		tv.volume(-12); // 볼륨 100-->-12-->0
+		tv.status(); 
+				
+		
+		System.out.println("");
+		System.out.println("*볼륨 up down--------------------------------------------------");
+		
+		tv.volume(50); // 볼륨 0-->50
+		tv.status(); 
+		
+		
+		// 볼륨확인-up
+		tv.volume(true); // 볼륨 50-->51
+		tv.status(); 
+		
+		// 볼륨확인-down
+		tv.volume(false); // 볼륨 51-->50
+		tv.status(); 
+		
+		// 볼륨확인-up 최대값
+		tv.volume(100); // 채널 51-->100
+		tv.status(); 
+		tv.volume(true); // 채널 100-->101-->100
+		tv.status();
+		
+		
+		// 볼륨확인-down 최소값
+		tv.volume(0); // 채널 100-->0
+		tv.status(); 
+		tv.volume(false); // 채널 0-->-1-->0
+		tv.status();	
+		
+				
+		*/
 	}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	
 
 }
