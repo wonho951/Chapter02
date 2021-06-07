21dc700c7b41137c1f1b317a2d50e00bdb0b6a7b
wonho <wonho951@gmail.com>
2021-06-07 10:10:19 +0900

    2021.06.07 오전수업

--- src/com/javaex/ex12/Person.java
+++ src/com/javaex/ex12/Person.java
@@ -39,11 +39,21 @@ public class Person {
 		this.age = age;
 	}
 	
-	//메소드-일반
-	
-	public void showInfo() {
-		System.out.println("이름 : " + name + " 나이 : " + age);
+	//데이터 확인용 (단축키사용)
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
 	}
+	
+	//메소드-일반	
+	public void showInfo() {  //화면 출력하는거(꾸미는거)라 생각하기.(만들기 힘듦, 지금은 걍 배우는단계라 단순 출력만 하는것.)
+		System.out.println("----------------");
+		System.out.println("이름 : " + name );
+		System.out.println("나이 : " + age);
+		System.out.println("----------------");
+	}
+
+
 	
 	
 	
--- src/com/javaex/ex12/PersonApp.java
+++ src/com/javaex/ex12/PersonApp.java
@@ -4,13 +4,18 @@ public class PersonApp {
 
 	public static void main(String[] args) {
 		
-		//테스트
+		//테스트		
+		Person p00 = new Person("정우성" , 46);		
 		
-		Person p00 = new Person("정우성" , 46);
+		//toSrting ---> 필드 , 생성자, 게터세터 잘 됐는지 확인하는용.
+		System.out.println(p00.toString()); 
+		
+		
+		p00.setAge(50);
+		System.out.println(p00.getAge());
+
 		
 		p00.showInfo();
-		
-
 	}
 
 }
